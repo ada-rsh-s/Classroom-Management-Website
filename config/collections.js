3f29c5526b5c39ddf4abc5941c03021061f0ac7b
Adarsh-S1 <adarshvillasuni@gmail.com>
2023-04-21 14:52:48 +0530

    deployed3

--- config/collections.js
+++ config/collections.js
@@ -4,14 +4,14 @@ module.exports = {
   NOTES_DOC_COLLECTION: "notesdoc",
   NOTES_VID_COLLECTION: "notesvideo",
   NOTES_U_VID_COLLECTION: "notesyoutube",
-  ASSIGNMENT_COLLECTION: "assignments",
+  ASSIGNMENT_COLLECTION: "assignments", 
   ATTENDANCE_COLLECTION: "attendance",
   ANNOUNCEMENT_COLLECTION: "announcements",
   EVENT_COLLECTION: "events",
   PHOTO_COLLECTION: "photos",
   PAID_COLLECTION: "paid",
   NOTI_COLLECTION: "notification",
-  HOLIDAY_COLLECTION: "holidays",
+  HOLIDAY_COLLECTION: "holidays", 
   CHAT_COLLECTION: "chats",
   PVT_CHAT_COLLECTION: "pvtchats"
 };
--- config/connection.js
+++ config/connection.js
@@ -5,15 +5,14 @@ const state = {
 module.exports.connect = function (done) {
   const url = "mongodb+srv://adarsh:adarsh@cluster1.6iztrde.mongodb.net/test";
   const dbname = "classroom";
-  mongoClient.connect(url, { useUnifiedTopology: true }, (err, data) => {
+  mongoClient.connect(url,  (err, data) => {
     if (err) return done(err);
     state.db = data.db(dbname);
   });
 
   done();
 };
-
+ 
 module.exports.get = function () {
-  console.log(state.db);
   return state.db;
 };
